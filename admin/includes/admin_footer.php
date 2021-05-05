4b2ceb4097737d60b343fcc32f25ff92a5a46f19
Hammed Adeleke <lekhad19@gmail.com>
2021-05-05 08:18:45 -0700

    Finalized CMS Blog system with procedural PHP

---
+++ admin/includes/admin_footer.php
@@ -0,0 +1,27 @@
+
+    </div>
+    <!-- /#wrapper -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+
+</body>
+</html>
+<script>
+    // alert("Hello");
+    $("#selectAllBoxes").click(function(event){
+        if(this.checked){
+            $('.checkBoxes').each(function(){
+                this.checked = true;
+            });
+        }else{
+            $('.checkBoxes').each(function(){
+                this.checked = false;
+            });
+        }
+    });
+</script>
